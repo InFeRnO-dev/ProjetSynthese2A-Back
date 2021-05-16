b9b1c043d0b6c46b422f583112edd337104990c6
Alexis <a.dufresne1999@gmail.com>
2021-05-16 20:22:38 +0200

    dao user-droits

--- class/item.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Item {
-    constructor(label, quantity, checked, contains, fk_id_user) {
-        this.label = label
-        this.quantity = quantity
-        this.checked = checked
-        this.contains = contains
-        this.fk_id_user = fk_id_user
-    }
-}
